aaaeb1e46cf20d06eedf741ced2e117975e19ba9
Yuri Vishnevsky <yurivish@gmail.com>
2023-06-20 16:00:04 -0400

    simplify

--- bit-structures/src/histogram.rs
+++ bit-structures/src/histogram.rs
@@ -1,4 +1,5 @@
 // https://github.com/pelikan-io/ccommon/blob/main/docs/modules/cc_histogram.rst
+// https://observablehq.com/d/35f0b601ed888da9
 // I want three histograms:
 // - naive raw pdf histogram backed by a CDF array, with an entry per bucket; can be incremented
 //   - maybe this is a builder (though we don't have those for anything else)
@@ -44,36 +45,39 @@ impl HistogramBuilder {
     //     Histogram { a: 0, b: 0, cdf }
     // }
 
+    // given a bin index, returns the lowest value that bin represents.
+    // pub fn low(i: u64) -> u64 {
+    //     if i < c {
+    //         i << a
+    //     } else {
+    //     }
+    // }
+
     pub fn bin_index(&self, value: u64) -> usize {
         let Self { a, b, c, .. } = self;
         if value < (1 << c) {
             // the bin width below the cutoff is 1 << a
             (value >> a) as usize
         } else {
-            // the log segment the value belongs to
+            // The log segment containing the value
             let v = value.ilog2();
 
-            // the number of bins per log segment (above the cutoff) = 2^b
-            let bins_per_log_segment = 1 << b;
+            // The linear section has twice as many bins as an individual log segment above the cutoff,
+            // for a total of 2*2^b bins below the cutoff.
+            // Above the cutoff, the value is preceded by `v - c` complete log sections, and each one
+            // has 2^b bins.
+            // Taken together, there are (v - c + 2) * 2^b bins preceding the v-th log segment.
+            let preceding_bins_before_log_segment = (v - c + 2) << b;
 
-            // the total number of linear bins (below the cutoff) = 2^(c-a) = 2^(b+1) = 2*2^b
-            let linear_bins = bins_per_log_segment << 1;
+            // The bin offset within the v-th log segment.
+            // - `value ^ (1 << v)` zeros the topmost (v-th) bit
+            // - `>> (v - b)` extracts the top `b` bits of the value, corresponding
+            //   to the bin index within the v-th log segment.
+            let preceding_bins_in_log_segment = ((value ^ (1 << v)) >> (v - b)) as u32;
 
-            // the number of log segments preceding the one containing `value`
-            let preceding_log_segments = v - c;
-            // could we compute bins_per_log_segment * preceding_log_segments directly
-            // as (value >> c) >> b?
-
-            // the bin offset within this log segment
-            // - the expression `(value - (1 << v))` un-sets the largest bit, or equivalently,
-            //   subtracts the portion of the value up to the start of the log segment,
-            //   leaving just the offset value within the log segment. could also be phrased
-            //   as `value & (value - 1)`. Or `value ^ (1 << b)`.
-            // - the shift by `(v - b)`  extracts the top `b` bits of the value, which correspond
-            //   to the bin index within this log segment (there are `2^b` bins per log segment).
-            let bin_offset = ((value ^ (1 << v)) >> (v - b)) as u32;
-
-            (linear_bins + bins_per_log_segment * preceding_log_segments + bin_offset) as usize
+            // there are 2^(b+1) = 2*2^b bins below the cutoff, and (v-c)*2^b bins between the cutoff
+            // and v-th log bin.
+            (preceding_bins_before_log_segment + preceding_bins_in_log_segment) as usize
         }
     }
 }
